faf23b95d9e22956db2edeb1188716c63394cfdf
atocco <alex.tocco88@gmail.com>
2017-11-28 14:44:34 -0500

    Adding home screen
    adding home screen and renaming project package to 'congaree'

---
+++ java/src/congaree/Controller.java
@@ -0,0 +1,4 @@
+package congaree;
+
+public class Controller {
+}
---
+++ java/src/congaree/Main.java
@@ -0,0 +1,23 @@
+package congaree;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        primaryStage.setTitle("SoundClout");
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
