7b02193bb3c36f63e7d703918bd29bd9639aeb64
fosdos <fosterclarksonwilliams@gmail.com>
2017-12-08 01:24:55 -0500

    Updating motor driver

--- RaspberryPiScripts/server/motorTest.py
+++ RaspberryPiScripts/server/motorTest.py
@@ -8,9 +8,15 @@ GPIO.setwarnings(False)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
+
 GPIO.output(22, GPIO.HIGH)
+GPIO.output(16, GPIO.HIGH)
+
+print "testing on"
 
 time.sleep(5)
+
+print "testing off"
 
 GPIO.output(22, GPIO.LOW)
 GPIO.cleanup()
